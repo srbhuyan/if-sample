4e41673316d7ae10474cc4608b42af2ef4b7fab4
Soumya Ranjan Bhuyan <sbhuyan@massivelyparallel.com>
2024-06-17 15:08:51 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+  int x = 10;
+  int y = 20;
+  int z = 30;
+
+  if(x>10){
+    for(int i=0;i<y;i++){
+    }
+    for(int j=0;j<z;z++){
+    }
+  } else if(x>20){
+  } else {}
+
+  return 0;
+}
